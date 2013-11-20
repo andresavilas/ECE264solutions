2342fd4a845998435a4b47d04d4306d6f91384e1
Andres Armando Avila <avila2@ecelinux39.ecn.purdue.edu>
2013-11-19 21:31:16 -0500

    Finished PA09

--- PA09/pa09.h
+++ PA09/pa09.h
@@ -1,16 +1,30 @@
 #ifndef PA09_H
 #define PA09_H
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct _huffnode {
-    int value; 
-    struct _huffnode * left;
-    struct _huffnode * right;
+  int value; 
+  struct _huffnode * left;
+  struct _huffnode * right;
 } HuffNode;
 
 typedef struct _stack {
-    struct _stack * next;
-    HuffNode * node;
+  struct _stack * next;
+  HuffNode * node;
 } Stack;
 
+typedef struct _bit {
+  FILE * fp;
+  unsigned char byte;
+} Bitfile;
+
+Stack * stack_push(Stack *,HuffNode *);
+Stack * stack_pop(Stack *);
+HuffNode * huff_char(FILE *);
+HuffNode * huff_bin(FILE *);
+HuffNode * create(int);
+void SparseArray_destroy(HuffNode *);
+void Huff_postOrderPrint(HuffNode *,FILE *);
 #endif
 
--- PA09/utility.c
+++ PA09/utility.c
@@ -1,1 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pa09.h"
+
+HuffNode * huff_char(FILE * fptr)
+{
+  int val;
+  Stack * st = NULL;
+  int command;
+  if(fptr == NULL)
+    {
+      return NULL;
+    }
+  while(fptr != NULL)
+    {
+      command = fgetc(fptr);
+      if(command == '1')
+	{
+	  val = fgetc(fptr);
+	  HuffNode * tree = create(val);
+	  st = stack_push(st,tree);
+	}
+      else
+	{
+	  HuffNode * A = st -> node;
+	  st = stack_pop(st);
+	  if(st == NULL)
+	    {
+	      return A;
+	    }
+	  else
+	    {
+	      HuffNode * B = st -> node;
+	      st = stack_pop(st);
+	      HuffNode * par = malloc(sizeof(HuffNode));
+	      par -> value = ' ';
+	      par -> right = A;
+	      par -> left = B;
+	      st = stack_push(st,par);
+	    }
+	}
+    }
+  return NULL;
+}
+
+HuffNode * huff_bin(FILE * fptr)
+{
+  unsigned char ch;
+  unsigned char buff;
+  int cmdloc = 1;
+  int ct = 0;
+  unsigned char buff_temp;
+  unsigned char val;
+  Stack * st = NULL;
+  const unsigned char mask [] = {0x80,0x40,0x20,0x10,0x08,0x04,0x02,0x01};
+  unsigned char command = 0;
+  ch = fgetc(fptr);
+  while(1)
+    {
+      command = (ch & mask[ct]);
+      if(command != 0)
+	{
+	  buff = fgetc(fptr);
+	  val = ch << cmdloc;
+	  buff_temp = buff >> (8 - cmdloc);
+	  val = val | buff_temp;
+	  HuffNode * tree = create(val);
+	  st = stack_push(st,tree);
+	  ch = buff;
+	}
+      else
+	{
+	  HuffNode * A = st -> node;
+	  st = stack_pop(st);
+	  if(st == NULL)
+	    {
+	      return A;
+	    }
+	  else
+	    {
+	      HuffNode * B = st -> node;
+	      st = stack_pop(st);
+	      HuffNode * par = malloc(sizeof(HuffNode));
+	      par -> value = ' ';
+	      par -> right = A;
+	      par -> left = B;
+	      st = stack_push(st,par);	      
+	     
+	    }  
+	}
+      	
+      if(cmdloc == 8)
+	{
+	  ch = fgetc(fptr);
+	  ct = 0;
+	  cmdloc = 1;
+	}
+      else
+	{
+	  cmdloc++;
+	  ct++;
+	}
+    }
+  return NULL;
+}
+
+HuffNode * create(int val)
+{
+  HuffNode * tree = malloc(sizeof(HuffNode));
+  tree -> value = val;
+  tree -> left = NULL;
+  tree -> right = NULL;
+  return tree;
+}
+
+Stack * stack_pop(Stack * head)
+{
+  if(head == NULL)
+    {
+      return NULL;
+    }
+  Stack * temp = head -> next;
+  free(head);
+  return temp;
+}
+
+Stack * stack_push(Stack * st,HuffNode * t)
+{
+  Stack * topnode = malloc(sizeof(Stack));
+  topnode -> node = t;
+  topnode -> next = st;
+  return topnode;
+}
+
+void SparseArray_destroy (HuffNode * array)
+{
+  if(array == NULL)
+    {
+      return;
+    }
+  SparseArray_destroy(array -> left);
+  SparseArray_destroy(array -> right);
+  free(array);
+}
+
+void Huff_postOrderPrint(HuffNode *tree,FILE * fptr)
+{
+    // Base case: empty subtree
+    if (tree == NULL) 
+      {
+	return;
+      }
+    // Recursive case: post-order traversal
+    // Visit left
+    fprintf(fptr,"Left\n");
+    Huff_postOrderPrint(tree->left,fptr);
+    fprintf(fptr,"Back\n");
+    // Visit right
+    fprintf(fptr,"Right\n");
+    Huff_postOrderPrint(tree->right,fptr);
+    fprintf(fptr,"Back\n");
+    // Visit node itself (only if leaf)
+    if (tree->left == NULL && tree->right == NULL) 
+      {
+	fprintf(fptr,"Leaf: %c\n", tree->value);
+      }
+}
+
+
 
